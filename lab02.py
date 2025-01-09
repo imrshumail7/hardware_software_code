3ba95f3abc3c202a52cbb4a7f989e0bb8e8224f3
imrshumail7 <imrshumail@gmail.com>
2025-01-09 11:40:19 -0500

    Update lab02.py

--- lab02.py
+++ lab02.py
@@ -1,21 +1,11 @@
-Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def main():
-...     name = "Professor Nedd"
-...     print("we want to know if you like programming!")
-...     print()
-...     print("Do you like programming{}?". format(name))
-...           
-SyntaxError: unterminated string literal (detected at line 5)
->>> def main():
-...     name = "Professor Nedd"
-...     print("we want to know if you like programming!")
-...     print()
-...     print("Do you like programming{}".format(name))
-...     answer = input()
-...     print("Great! You said {}?".format(answer))
-...     print("Let's start learning some Python today")
-... 
-...     
->>> if __name__ == "__main__":
-...     main()
+def main():
+    name = "Professor Nedd"
+    print("we want to know if you like programming!")
+    print()
+    print("Do you like programming{}".format(name))
+    answer = input()
+    print("Great! You said {}?".format(answer))
+    print("Let's start learning some Python today")
+  
+if __name__ == "__main__":
+    main()
